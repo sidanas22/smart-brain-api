3e830a21a8a7abba6badf64a09035a904244ef58
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-19 21:00:36 +0500

    rev

--- controllers/register.js
+++ controllers/register.js
@@ -70,22 +70,26 @@ const handleRegister = (req, res, db, bcrypt, crypto) => {
                                     var chose = 'insert into user_sessions where (session_id, expired, user_id) values(' + random_string + ',' + ' false,( select id from users where id = ' + user[0].id.toString() + '));'
 
                                     //const sub_query = trx.select('user_id').from('users').where()
-                                   
+
 
                                     return trx('user_sessions').insert({
                                         session_id: random_string,
                                         expired: false,
-                                        user_id : user[0].id
-                                    }).then(success => {  res.status(200).json({
-                                        random_string: random_string
-                                    }) })
-                                    
+                                        user_id: user[0].id
+                                    }).then(success => {
+                                        res.status(200).json({
+                                            random_string: random_string
+                                        })
+
+                                        return success
+                                    })
+
                                 })
                                 //trx.raw(chose);
                                 // console.log("inside transaction this has worked too");
                                 // return trx;
 
-                                
+
                             });
 
 
@@ -121,10 +125,10 @@ const handleRegister = (req, res, db, bcrypt, crypto) => {
         //     )
 
     });
-// });
+    // });
 
 
-console.log("after hash function");
+    console.log("after hash function");
 
 
 
