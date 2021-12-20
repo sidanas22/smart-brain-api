28975d89730a416364743fcab613988be84f8b14
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-20 12:11:29 +0500

    logout

--- controllers/logout.js
+++ controllers/logout.js
@@ -5,24 +5,28 @@ const express = require('express');
 
 const handle_logout = (req, res, db) => {
     //console.log("When logging out. User Id is ", req.user.id);
+    flag = false;
     console.log("When logging out Session Id is ", req.body.session_id);
-    
-    db.transaction( function (trx)
-    {
-        db('user_sessions').transacting(trx).where('session_id', req.body.session_id).del()
-        .then(()=>{
-            trx.commit();
-           return res.status(200).json({logged_out: true});
-            
-        })
-        .catch(trx.rollback);
+
+    db.transaction(function (trx) {
+        return db('user_sessions').transacting(trx).where('session_id', req.body.session_id).del()
+            .then((data) => {
+                flag = true;
+                res.status(200).json({ logged_out: true });
+                trx.commit();
+
+
+            })
+            .catch((err)=>{ flag=false; trx.rollback();});
     });
-    
+
     //db('user_sessions').where('session_id', req.body.session_id).del();
     //res.clearCookie(process.env.SESS_NAME);
-    return res.status(200).json({logged_out: false});
+    if (flag) {
+        return res.status(200).json({ logged_out: false });
+    }
 }
 
-module.exports={
+module.exports = {
     handle_logout: handle_logout
 }
