d91b819a2a589b7cbe858e3db5de46c59719ec53
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-20 12:31:16 +0500

    logout

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,87 +1,83 @@
 const redirect_root = (req, res, next, db) => {
-   
-if(req.body.session_id)
-{
-    db('user_sessions').where({
-        expired: false,
-        session_id: req.body.session_id,
 
-    }).select('session_id')
-    .then(data =>{
-        console.log(data);       
-        res.redirect('/home');
-    } )
-    .catch(err => {
+    if (req.body.session_id) {
+        return db('user_sessions').where({
+            expired: false,
+            session_id: req.body.session_id,
+
+        }).select('session_id')
+            .then(data => {
+                console.log(data);
+                res.redirect('/home');
+            })
+            .catch(err => {
+                res.status(200).json({ bogged_out: true });
+            })
+    }
+
+    else {
         return res.status(200).json({ logged_out: true });
-    })
-}
 
-else
-{
-    return res.status(200).json({ logged_out: true });
-    
-    
-}
+
+    }
 
 
 }
 
 const redirect_home = (req, res, next, db) => {
-    
-if(req.body.session_id)
-{
-    db('user_sessions').where({
-        expired: false,
-        session_id: req.body.session_id,
 
-    }).select('session_id')
-    .then(data =>{
-        console.log(data);
-       
-        res.redirect('/home');
-    } )
-    .catch(err => {
-        return next();
-        
-    })
-}
+    if (req.body.session_id) {
+        return db('user_sessions').where({
+            expired: false,
+            session_id: req.body.session_id,
 
-else
-{
-    next()
-    
-}
+        }).select('session_id')
+            .then(data => {
+                console.log(data);
+
+                res.redirect('/home');
+            })
+            .catch(err => {
+                next();
+
+            })
+    }
+
+    else {
+        next()
+
+    }
 
 
 
 }
 
 const redirect_signin = (req, res, next, db) => {
-    
-    if(req.body.session_id)
-    {
-        db('user_sessions').where({
-        expired: false,
-        session_id: req.body.session_id,
 
-    }).select('session_id')
-    .then(data =>{
-        console.log(data);
-        //res.redirect('/home');
-        return next();
-    } )
-    .catch(err => {
-        return res.status(200).json({ logged_out: true });
-        //return next();
-    })
+    if (req.body.session_id) {
+        return db('user_sessions').where({
+            expired: false,
+            session_id: req.body.session_id,
+
+        }).select('session_id')
+            .then(data => {
+                console.log(data);
+                //res.redirect('/home');
+                next();
+            })
+            .catch(err => {
+                //return res.status(200).json({ logged_out: true });
+                res.send("error there was")
+                //return next();
+            })
     }
-    else{
+    else {
         return res.status(200).json({ logged_out: true });
     }
-    
+
 }
 
-module.exports={
+module.exports = {
     redirect_signin,
     redirect_home,
     redirect_root
