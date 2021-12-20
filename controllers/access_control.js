aae3427d222e23eb56c261a8815f5d03bbe1e495
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-20 20:51:25 +0500

    access_control

--- controllers/access_control.js
+++ controllers/access_control.js
@@ -1,36 +1,97 @@
 
 
 const handle_access_control = (req, res, db) => {
-    const { session_id, user_id, new_val } = req.body;
+    const { session_id, target_user_id, new_val } = req.body;
 
 
-    db.select('user_id','roles').from('user_sessions').where('session_id', '=', session_id)
-        .then(user => {
+   return db.select('user_id').from('user_sessions').where('session_id', '=', session_id)
+        .then(user_id => {
 
-            console.log(user[0]);
+            console.log(user_id[0].id);
 
-            //////////////////////////////////
-            // db.transaction(function (trx) {
+            
 
-            //         trx.select('roles')
+            return db.select('roles').from('users').where('id', '=', user_id[0].id)
+                .then(role => {
+                    //role[0].roles
+                    var my_role = role[o].roles;
 
-            //     return trx
-            //         .insert({ name: 'Old Books' }, 'id')
-            //         .into('catalogues')
-            //         .then(function (ids) {
-            //             books.forEach((book) => book.catalogue_id = ids[0]);
-            //             return trx('books').insert(books);
-            //         });
-            // })
-            //     .then(function (inserts) {
-            //         console.log(inserts.length + ' new books saved.');
-            //     })
-            //     .catch(function (error) {
-            //         // If we get here, that means that neither the 'Old Books' catalogues insert,
-            //         // nor any of the books inserts will have taken place.
-            //         console.error(error);
-            //     });
-            //////////////////////////////
+                   return db.select('roles').from('users').where('id', '=', target_user_id)
+                        .then(target_id => {
+
+                            if (my_role >= target_id[0].id && my_role >= 20 && new_val <= my_role ) {
+                                //////////////////////////////////
+                                return db.transaction(function (trx) {
+
+                                 
+                                    return trx('users')
+                                        .where('id','=', target_id[0].id)
+                                        .update(
+                                            {
+                                                roles: new_val
+                                            }
+                                        )
+                                        .then(function (ids) {
+                                            // books.forEach((book) => book.catalogue_id = ids[0]);
+                                            //return trx('books').insert(books);
+                                            return res.status(200).json({
+                                                valueUpdated: true
+                                            })
+                                        })
+                                        .catch( err => {
+                                            return res.status(400).json({
+                                                valueUpdated: false,
+                                                error : err.message
+                                            })  
+                                        });
+                                })
+                                    .then(function (inserts) {
+                                        return res.status(200).json({
+                                            valueUpdated: true
+                                        })
+                                    })
+                                    .catch(function (error) {
+                                        return res.status(400).json({
+                                            valueUpdated: false,
+                                            error : err.message
+                                        })
+                                        console.error(error);
+                                    });
+                                //////////////////////////////
+
+                            }
+
+                            else{
+                                return res.json("not allowed for current role")
+                            }
+                        })
+                        .catch(err => {
+                            return res.status(400).json({
+                                valueUpdated: false,
+                                error : err.message
+                            })
+                        })
+
+
+
+
+
+
+                })
+                .catch(err => {
+                    return res.status(400).json({
+                        valueUpdated: false,
+                        error : err.message
+                    })
+                })
+
+
+        })
+        .catch(err => {
+            return res.status(400).json({
+                valueUpdated: false,
+                error : err.message
+            })
         })
 }
 
