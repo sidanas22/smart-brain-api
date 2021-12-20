e30c4b2757a1177362b9ec62043778e799878a67
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-20 16:32:06 +0500

    logout  details changed

--- controllers/logout.js
+++ controllers/logout.js
@@ -5,45 +5,49 @@ const express = require('express');
 
 const handle_logout = (req, res, db) => {
     //console.log("When logging out. User Id is ", req.user.id);
-    if(req.body.session_id)
-    {
+    if (req.body.session_id) {
 
-    flag = false;
-   
-    db('user_sessions').select('session_id').where('session_id','=', req.body.session_id)
-    .then(console.log)
-    .catch(err => {
-        return res.json({
-            logged_in: false
-        })
-    })
+        flag = false;
+        flag2 = false;
 
-
-    console.log("When logging out Session Id is ", req.body.session_id);
-
-    db.transaction(function (trx) {
-        return db('user_sessions').transacting(trx).where('session_id', req.body.session_id).del()
-            .then((data) => {
-                flag = true;
-                res.status(200).json({ logged_out: true });
-                trx.commit();
-
-
+        db('user_sessions').select('session_id').where('session_id', '=', req.body.session_id)
+            .then(console.log)
+            .catch(err => {
+                flag2 = true;
             })
-            .catch((err)=>{ flag=false; trx.rollback(); res.status(200).json({ logged_out: false }); });
-    });
 
-    //db('user_sessions').where('session_id', req.body.session_id).del();
-    //res.clearCookie(process.env.SESS_NAME);
-   
-    if (flag) {
-        return res.status(200).json({ logged_out: false });
+        if (flag2) {
+            return res.json({
+                logged_in: false
+            })
+        }
+
+
+        console.log("When logging out Session Id is ", req.body.session_id);
+
+        db.transaction(function (trx) {
+            return db('user_sessions').transacting(trx).where('session_id', req.body.session_id).del()
+                .then((data) => {
+                    flag = true;
+                    res.status(200).json({ logged_out: true });
+                    trx.commit();
+
+
+                })
+                .catch((err) => { flag = false; trx.rollback(); res.status(200).json({ logged_out: false }); });
+        });
+
+        //db('user_sessions').where('session_id', req.body.session_id).del();
+        //res.clearCookie(process.env.SESS_NAME);
+
+        if (flag) {
+            return res.status(200).json({ logged_out: false });
+        }
     }
-}
 
-else{
-    return res.status(400).json({ logged_in: false})
-}
+    else {
+        return res.status(400).json({ logged_in: false })
+    }
 
 }
 
