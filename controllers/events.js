6334a8f9482d267992e47c5cac7014eea0340fd8
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-21 21:34:28 +0500

    hello world

--- controllers/events.js
+++ controllers/events.js
@@ -12,6 +12,81 @@
 //     "event_description": "DETAILS OF EVENT"
 // }
 
+const handleApproveEvent = (req, res, db) => {
+
+    const {
+        session_id,
+        event_name,
+        event_start_date,
+        event_end_date,
+        event_start_time, //HH12:MI
+        event_start_time_period,
+        event_end_time,
+        event_end_time_period,
+        event_venue,
+        event_description
+
+    } = req.body;
+
+    const auth_role = [40];
+
+    return db.select('user_id').from('user_sessions').where('session_id', '=', session_id)
+        .then(user => {
+
+            if (user) {
+
+                return db.select('roles').from('users').where({
+                    id: user[0].user_id
+                })
+                    .then(role => {
+
+                        if (role[0].roles == auth_role[0]) {
+
+                            console.log("helloworld");
+                            return db('event').insert({
+                                event_name: event_name,
+                                event_start_date: event_start_date,
+                                event_end_date: event_end_date,
+                                event_start_time: event_start_time,
+                                event_start_time_period: event_start_time_period,
+                                event_end_time: event_end_time,
+                                event_end_time_period: event_end_time_period,
+                                event_venue: event_venue,
+                                event_description: event_description,
+                                event_head: user[0].user_id
+                            })
+                                .then(success => {
+                                    return res.status(200).json({ eventCreated: true });
+                                })
+                                .catch(err => {
+                                    return res.status(400).json({ error: err.message });
+                                })
+
+                        }
+
+                        else {
+                            return res.json({
+                                error: "noAuth"
+                            })
+                        }
+
+                    })
+                    .catch(err => {
+                        return res.status(400).json({ error: err.message });
+                    })
+
+
+            }
+            
+
+        })
+        .catch(err => {
+
+            return res.status(400).json({ error: err.message });
+        })
+}
+
+
 const handleCreateEvent = (req, res, db) => {
 
     const {
@@ -68,6 +143,8 @@ const handleCreateEvent = (req, res, db) => {
             return res.status(400).json({ error: err.message });
         })
 }
+
+
 
 module.exports = {
     handleCreateEvent
