7f0e89472d59042f5bd38c745af64ccac01c7c2e
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-23 00:46:24 +0500

    induction js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(session);
+//const KnexSessionStore = require('connect-session-knex')(session);
 
 
 const crypto = require('crypto');
@@ -63,11 +63,17 @@ app.use(cors());
 //     origin: '*'
 // }));
 
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
-});
+  })
 
 
 
