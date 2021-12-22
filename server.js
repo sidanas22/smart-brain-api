f3f644667af6642ffead0fb29d1cb1c6d849ee8c
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-23 01:45:20 +0500

    induction js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header('Access-Control-Allow-Origin', 'http://localhost')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:30')
     next();
   })
 
