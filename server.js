676c00e5c2df0096f460f1220b86ef4cb9246e90
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2021-12-23 15:13:00 +0500

    dev

--- server.js
+++ server.js
@@ -59,15 +59,15 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
  
   
   
